958be4c0b16ce409138613740f2807032912dc94
Nestor <nestorbardel@gmail.com>
2025-04-27 23:07:05 -0300

    Se agrego la carpeta al repo

---
+++ IntroCsharpBackend/Program.cs
@@ -0,0 +1,29 @@
+﻿// Sale sale = new Sale();
+// var sale = new Sale();
+Sale sale = new(15);
+string message = sale.GetInfo();
+Console.WriteLine(message);
+
+class SaleWithTax : Sale
+{
+    public SaleWithTax(decimal total) : base(total)
+    {
+        
+    }
+}
+
+class Sale
+{
+    public decimal Total { get; set; }
+    private decimal _some;
+
+    public Sale(decimal total)
+    {
+        this.Total = total;
+    }
+
+    public string GetInfo()
+    {
+        return "El Total es: " + this.Total;
+    }
+}
